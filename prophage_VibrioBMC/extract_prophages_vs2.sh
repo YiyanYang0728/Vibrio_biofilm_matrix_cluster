c165c0d6bdba9b494bc9a2d8399e1a785ac7cb33
Yiyan Yang <yiyanyang0728@gmail.com>
2024-08-19 11:13:21 -0400

    update data

--- prophage_VibrioBMC/extract_prophages_vs2.sh
+++
@@ -1,8 +0,0 @@
-gid=$1
-outdir=${2:-$PWD}
-# get prophage numbers & get contig, start and end pos for each phage
-file=/data/yangy34/projects/Vibrio_phages/run_vs2/vs2_out/${gid}*/final-viral-boundary.tsv
-tail -n+2 ${file} | cut -f1,4,5,17,18,29,31 > ${outdir}/${gid}_prophages_vs2.tsv
-ct=`cat ${outdir}/${gid}_prophages_vs2.tsv|wc -l`
-echo -e "$gid\t$ct"
-#rm ${outdir}/${gid}_prophages_vs2.tsv
--- prophage_VibrioBMC/get_prophage_fas.sh
+++
@@ -1,7 +0,0 @@
-ctg=$1
-start=$2
-end=$3
-outdir=${4:-$PWD}
-gid=`echo ${ctg} | cut -f1-2 -d_`
-# echo $gid
-cat /gpfs/gsfs12/users/yangy34/projects/vibrio_YJ_v5/VPS_cluster/genomes/fna/${gid}.fna|seqkit fx2tab|fgrep -w ${ctg}|seqkit tab2fx |seqkit subseq -r ${start}:${end} | seqkit fx2tab | awk -v p_ctg=${ctg} -v ns=${start} -v ne=${end} '{print p_ctg"_"ns"-"ne"\t"$2}' | seqkit tab2fx > ${outdir}/${ctg}_${start}_${end}_prophage.fasta
